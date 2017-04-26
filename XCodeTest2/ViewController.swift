bf75fcb647dc825eff0dcaddf090312ec7ed169f
Kashif Imran <kashifimran@outlook.com>
2017-04-25 21:03:20 -0700

    Initial Commit

---
+++ XCodeTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCodeTest2
+//
+//  Created by Kashif Imran on 4/25/17.
+//  Copyright © 2017 Kashif Imran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
